0fd59db218d468ca33f9a3067efb6df9eb3d37c9
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-05-06 12:47:15 -0500

    punto 2 terminado

--- Punto2/Piloto.py
+++ Punto2/Piloto.py
@@ -1,7 +1,6 @@
 class Piloto:
     def __init__(self):
         self.__nombre = None
-        self.__salarioAnual = None
 
     @property
     def nombre(self):
@@ -10,14 +9,5 @@ class Piloto:
     @nombre.setter
     def nombre(self, dato):
         self.__nombre = dato
-
-    
-    @property
-    def salarioAnual(self):
-        return self.__salarioAnual
-
-    @salarioAnual.setter
-    def salarioAnual(self, dato):
-        self.__salarioAnual = dato
 
     
